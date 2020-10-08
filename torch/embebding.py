43d67bf175ecc08dc13953ededce3ec5b780bb07
zero <kingstarcraft@foxmail.com>
2020-10-08 11:13:37 +0800

    fix bug.

--- torch/embebding.py
+++ torch/embebding.py
@@ -2,7 +2,7 @@ import torch
 
 
 class Position(torch.nn.Module):
-    def __init__(self, shape, channels):
+    def __init__(self, shape, channels, mean=0, std=0.02):
         '''
         :param shape: the shape of position
         :param channels: the channel of position
@@ -11,7 +11,7 @@ class Position(torch.nn.Module):
         if isinstance(shape, int):
             shape = (shape,)
         self.weight = torch.nn.Parameter(torch.Tensor(channels, *shape))
-        torch.nn.init.trunc_normal_(self.weight, mean=0, std=0.02, )
+        torch.nn.init.trunc_normal_(self.weight, mean=mean, std=std, a=mean - 2 * std, b=mean + 2 * std)
 
     def forward(self, inputs, start=None):
         '''
@@ -23,14 +23,14 @@ class Position(torch.nn.Module):
         offset = len(input_shape) - len(weight_shape)
         assert offset >= 0
         assert input_shape[offset] == weight_shape[0]
-        offset = offset+1
+        offset = offset + 1
         weight_shape = weight_shape[1:]
-        if shape is None:
-            shape = [0 for _ in weight_shape]
-        assert len(shape) == len(weight_shape)
-        slices = []
-        for id in range(0,  len(weight_shape)):
-            end = start[id]+weight_shape[id]
-            assert end <= input_shape[offset + id]
-            slices.append(slice(start[id], end)
-        return inputs + self.weight[..., *slices]
+        if start is None:
+            start = [0 for _ in weight_shape]
+        assert len(start) == len(weight_shape)
+        slices = [slice(None)]
+        for id in range(0, len(weight_shape)):
+            end = start[id] + input_shape[offset + id]
+            assert end <= weight_shape[id]
+            slices.append(slice(start[id], end))
+        return inputs + self.weight[slices]
