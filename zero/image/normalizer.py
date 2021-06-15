9d0fcfb7df2bd2efe9c33f1737d9cf1034724797
zero <kingstarcraft@foxmail.com>
2021-06-15 18:33:16 +0800

    add target normalizer

--- zero/image/normalizer.py
+++ zero/image/normalizer.py
@@ -3,11 +3,12 @@ from . import conversion
 
 
 class ReinhardNormal:
-    def __init__(self, from_color, to_color, ):
+    def __init__(self, from_color, to_color, target=None):
         self._from_color = from_color
         self._to_color = to_color
+        self._target = target
 
-    def __call__(self, inputs, dst, src=None, offset=None):
+    def __call__(self, inputs, dst=None, src=None, offset=None):
         if src is None:
             shape = inputs.shape
             temps = np.reshape([-1, shape[-1]])
@@ -16,7 +17,8 @@ class ReinhardNormal:
             src = np.array(src[0]), np.array(src[1])
         if offset is not None:
             src = src[0] + offset[0], src[1] + offset[1]
-
+        if dst is None:
+            dst = self._target
         outputs = self._from_color(inputs)
         outputs = (outputs - src[0]) / src[1]
         outputs = outputs * dst[1] + dst[0]
