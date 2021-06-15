e78458c41dd90b602416bd99059aa878e4283a6d
zero <kingstarcraft@foxmail.com>
2021-06-15 16:02:00 +0800

    add offset for normalizer

--- zero/image/normalizer.py
+++ zero/image/normalizer.py
@@ -7,13 +7,16 @@ class ReinhardNormal:
         self._from_color = from_color
         self._to_color = to_color
 
-    def __call__(self, inputs, dst, src=None):
+    def __call__(self, inputs, dst, src=None, offset=None):
         if src is None:
             shape = inputs.shape
             temps = np.reshape([-1, shape[-1]])
             src = np.mean(temps, axis=0), np.std(temps, axis=0)
         else:
             src = np.array(src[0]), np.array(src[1])
+        if offset is not None:
+            src = src[0] + offset[0], src[1] + offset[1]
+
         outputs = self._from_color(inputs)
         outputs = (outputs - src[0]) / src[1]
         outputs = outputs * dst[1] + dst[0]
