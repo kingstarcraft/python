53d126e58539af90108dd454852f92c039284116
zero <kingstarcraft@foxmail.com>
2021-06-18 11:30:20 +0800

    add nms for box

--- zero/box.py
+++ zero/box.py
@@ -64,3 +64,31 @@ def filter_box(boxes, overlap=0.9):
             mask[x] = False
     return boxes[mask]
 
+
+def nms(dets, thresh):
+    """
+    nms
+    :param dets: ndarray [x1,y1,x2,y2,score]
+    :param thresh: int
+    :return: list[index]
+    """
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    order = dets[:, 4].argsort()[::-1]
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+        over = (w * h) / (area[i] + area[order[1:]] - w * h)
+        index = np.where(over <= thresh)[0]
+        order = order[index + 1]
+    return keep
